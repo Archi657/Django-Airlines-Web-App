9f5d7b647870e8e0f019287fd447e330c9fa9777
Julian Gomez <juliandr544@gmail.com>
2021-11-08 20:42:16 -0500

    first commit

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name="index"),
-    path("login", views.login_view, name="login"),
+    path("home", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout")
 ]
