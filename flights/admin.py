74cb5294ea198305cc3e4ccf21c1e993e590a082
julian <juliandr544@gmail.com>
2023-02-03 19:39:22 -0500

    +Airplane

--- flights/admin.py
+++ flights/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Airport, Country, Flight
+from .models import Airport, Country, Flight, Airplane
 
 admin.site.register(Airport)
+admin.site.register(Airplane)
 admin.site.register(Country)
 admin.site.register(Flight)
--- flights/models.py
+++ flights/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Country(models.Model):
     name = models.CharField(max_length=15)
@@ -10,7 +11,7 @@ class Airport(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="airports")
 
     def __str__(self):
-        return f"{self.name} from {self.country}"
+        return f"{self.name} ({self.country})"
 
 class Flight(models.Model):
     origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
@@ -19,6 +20,27 @@ class Flight(models.Model):
     departure = models.DateTimeField(auto_now=True) # save it every time, allowing to update the hour.
     arrival = models.DateTimeField(auto_now=True) 
 
+    airplane = models.ForeignKey('Airplane', on_delete=models.SET_NULL, null=True, related_name="flights",blank=True)
+
+    def __str__(self):
+        return f"{self.origin} to {self.destination}"
+
 class Airplane(models.Model):
-    # user foerign key to each seat.
-    #seat1a = 
+    flight = models.OneToOneField(Flight, on_delete=models.CASCADE, related_name="plane", primary_key=True)
+
+    a1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seata1", blank=True)
+    a2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seata2", blank=True)
+    a3 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seata3", blank=True)
+    a4 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seata4", blank=True)
+    b1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatb1", blank=True)
+    b2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatb2", blank=True)
+    b3 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatb3", blank=True)
+    b4 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatb4", blank=True)
+    c1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatc1", blank=True)
+    c2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatc2", blank=True)
+    c3 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatc3", blank=True)
+    c4 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="seatc4", blank=True)
+
+    def __str__(self):
+        return f"From : {self.flight.origin} to {self.flight.destination}"
+
