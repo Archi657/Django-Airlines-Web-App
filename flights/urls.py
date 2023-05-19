c61bfa3c3917d2e46adcffba0ae3dcc4c19211ad
Julian <40327956+Archi657@users.noreply.github.com>
2023-05-19 11:23:20 -0500

    Static files added
    Static files added
    - Images and Styles
    - Add flights view/url and .html
    - Change <ul> to show flights
    (Need to fix the styles of the <ul>)

--- flights/urls.py
+++ flights/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path("flights/", views.flights, name="flights"),
 ]
