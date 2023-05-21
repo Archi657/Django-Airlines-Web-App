f5d5703e8f4a04907535e9d7b424322956cf8ba2
omar6635 <omar66@schule.bremen.de>
2023-05-22 00:06:32 +0200

    add horizontal filter to passengers field in admin

--- flights/admin.py
+++ flights/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Airport, Country, Flight, Airplane
 
+
+class FlightAdmin(admin.ModelAdmin):
+    filter_horizontal = ('passengers',)
+
+
 admin.site.register(Airport)
 admin.site.register(Airplane)
 admin.site.register(Country)
-admin.site.register(Flight)
+admin.site.register(Flight, FlightAdmin)
