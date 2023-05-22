ea0f0b7d6451fe910baf88a6684f1560e204a63b
Julian <40327956+Archi657@users.noreply.github.com>
2023-05-21 22:03:00 -0500

    Created FlightDesc
    Added Flight visualization, creating the html file and adding the path in urls and def in views.

--- flights/urls.py
+++ flights/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path("flights/", views.flights, name="flights"),
-]
+    path("about/", views.flights, name="about"),
+    path("<int:flight_id>", views.flight, name="flight"),
+    #path("<int:flight_id>/book", views.book, name="book")
+]   
--- flights/views.py
+++ flights/views.py
@@ -5,10 +5,30 @@ from .models import Country, Airport, Flight
 def home(request):
     countries = Country.objects.all()
     flights = Flight.objects.all()
-    return render(request, 'flights/home.html', {'countries':countries, 'flights':flights})
+    return render(request, 'home.html', {'countries':countries, 'flights':flights})
 
 def flights(request):
     return render(request,"flights/flights.html",{
         "flights": Flight.objects.all(),
         'countries': Country.objects.all() 
     })
+
+def flight(request, flight_id):
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight" : flight,
+        "passengers": flight.passengers.all(),
+        #"non_passengers": Passenger.objects.exclude(flights=flight).all()
+    })
+
+#def about(request):
+#    return render(request, 'aboutus.html')
+
+
+
+#def book(request, flight_id):
+#    if request.method == "POST":
+#        flight = Flight.objects.get(pk=flight_id)
+#        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+#        passenger.flights.add(flight)
+#        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
